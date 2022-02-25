a5517a456607a4b0af42c779b9cccb2591e2af39
Sing0424 <ivan4680@gmail.com>
2022-02-25 19:02:59 +0800

    add file

---
+++ Assets/C#/TEST.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour {
+
+    private Animator _animator;
+    private Collider other;
+
+	// Use this for initialization
+	void Start () {
+        _animator = GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("open", true);
+        }
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        {
+            if (other.tag == "Player")
+            {
+                _animator.SetBool("open", true);
+            }
+            else
+            {
+                _animator.SetBool("open", false);
+            }
+        }
+
+    }
+}
---
+++ Assets/Java/Pickup.js
@@ -0,0 +1,17 @@
+﻿var onhand : Transform;
+
+function  Update () {
+
+}
+
+function OnMouseDown () {
+	GetComponent.<Rigidbody>().useGravity = false;
+	this.transform.position = onhand.position;
+	this.transform.parent = GameObject.Find("player").transform;
+	this.transform.parent = GameObject.Find("Camera").transform;
+}
+
+function OnMouseUp (){
+	this.transform.parent = null;
+	GetComponent.<Rigidbody>().useGravity = true;
+}
